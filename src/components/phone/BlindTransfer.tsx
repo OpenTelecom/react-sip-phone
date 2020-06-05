3dc3bbddc3c02934a63e5af96e4d7a729018f9ba
mxxnus <lxnkzr@gmail.com>
2020-06-05 15:05:04 -0400

    feat(session-control-attendedtransfer): attendedtransfer module

---
+++ src/components/phone/BlindTransfer.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react'
+import { connect } from 'react-redux'
+
+import { Session, UserAgent } from 'sip.js'
+import {
+  blindTransferRequest,
+  blindTransferSuccess,
+  blindTransferFail
+} from '../../actions/sipSessions'
+
+interface Props {
+  session: Session
+  userAgent: UserAgent
+  destination: string
+  blindTransferRequest: Function
+  blindTransferSuccess: Function
+  blindTransferFail: Function
+}
+
+class BlindTransfer extends React.Component<Props> {
+  blindTransferCall() {
+    this.props.blindTransferRequest
+    const target = UserAgent.makeURI(
+      `sip:${this.props.destination}@sip.reper.io;user=phone`
+    )
+    console.log(target)
+    if (target) {
+      this.props.session.refer(target)
+      this.props.blindTransferSuccess
+    } else {
+      this.props.blindTransferFail
+    }
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <button onClick={() => this.blindTransferCall()}>
+          Blind Transfer Call
+        </button>
+      </React.Fragment>
+    )
+  }
+}
+
+const mapStateToProps = (state: any) => ({
+  stateChanged: state.sipSessions.stateChanged,
+  sessions: state.sipSessions.sessions,
+  userAgent: state.sipAccounts.userAgent,
+  onHold: state.sipSessions.onHold
+})
+const actions = {
+  blindTransferRequest,
+  blindTransferSuccess,
+  blindTransferFail
+}
+
+export default connect(mapStateToProps, actions)(BlindTransfer)
--- src/components/phone/Phone.tsx
+++ src/components/phone/Phone.tsx
@@ -1,21 +1,12 @@
 import * as React from 'react'
 import Dialpad from './Dialpad'
 import { connect } from 'react-redux'
-import { Session, SessionState, UserAgent, Inviter } from 'sip.js'
+import { Session, SessionState, UserAgent } from 'sip.js'
 import { endCall } from '../../actions/sipSessions'
-import { phoneStore } from '../../index'
 import Hold from './Hold'
 import Mute from './Mute'
 import BlindTranfer from './BlindTransfer'
-
-import {
-  SIPSESSION_STATECHANGE,
-  NEW_SESSION,
-  CLOSE_SESSION,
-  SIPSESSION_ATTENDED_TRANSFER_REQUEST,
-  SIPSESSION_ATTENDED_TRANSFER_FAIL,
-  SIPSESSION_ATTENDED_TRANSFER_SUCCESS
-} from '../../actions/sipSessions'
+import AttendedTransfer from './AttendedTransfer'
 
 interface Props {
   session: Session
@@ -28,9 +19,7 @@ class Phone extends React.Component<Props> {
     dialpadOpen: true,
     ended: false,
     transferDialString: '',
-    audio: null,
-    attendedTransferSession: null,
-    attendedTransferSessionPending: null
+    audio: null
   }
 
   componentDidUpdate(newProps: Props) {
@@ -59,102 +48,6 @@ class Phone extends React.Component<Props> {
     }, 5000)
   }
 
-  attendedTransferCall() {
-    // if (!this.state.onHold) {
-    //   this.hold()
-    // }
-    console.log(this.props.userAgent)
-    const target = UserAgent.makeURI(
-      `sip:${this.state.transferDialString}@sip.reper.io;user=phone`
-    )
-    phoneStore.dispatch({
-      type: SIPSESSION_ATTENDED_TRANSFER_REQUEST
-    })
-    if (target) {
-      const inviter = new Inviter(this.props.userAgent, target)
-      const outgoingSession: Session = inviter
-      phoneStore.dispatch({ type: NEW_SESSION, payload: outgoingSession })
-      outgoingSession.stateChange.addListener((newState: SessionState) => {
-        switch (newState) {
-          case SessionState.Initial:
-          case SessionState.Establishing:
-            phoneStore.dispatch({
-              type: SIPSESSION_STATECHANGE,
-              payload: { state: newState, id: outgoingSession.id }
-            })
-            //add new session to local state
-            this.setState({ attendedTransferSessionPending: outgoingSession })
-            break
-          case SessionState.Established:
-            this.setState({ attendedTransferSession: outgoingSession })
-            //removes ability to cancel transfer
-            this.setState({ attendedTransferSessionPending: false })
-            phoneStore.dispatch({
-              type: SIPSESSION_STATECHANGE,
-              payload: { state: newState, id: outgoingSession.id }
-            })
-            this.setState({ transferDialString: '' })
-            break
-          case SessionState.Terminating:
-            phoneStore.dispatch({
-              type: SIPSESSION_STATECHANGE,
-              payload: { state: newState, id: outgoingSession.id }
-            })
-            break
-          case SessionState.Terminated:
-            phoneStore.dispatch({
-              type: SIPSESSION_STATECHANGE,
-              payload: { state: newState, id: outgoingSession.id }
-            })
-            phoneStore.dispatch({
-              type: CLOSE_SESSION,
-              payload: outgoingSession.id
-            })
-            break
-          default:
-            console.log(`Unknown session state change: ${newState}`)
-            break
-        }
-      })
-      outgoingSession.invite().catch((error: Error) => {
-        phoneStore.dispatch({
-          type: SIPSESSION_ATTENDED_TRANSFER_FAIL
-        })
-        console.log(error)
-      })
-    } else {
-      phoneStore.dispatch({
-        type: SIPSESSION_ATTENDED_TRANSFER_FAIL
-      })
-    }
-  }
-
-  //refers the session in local state
-  connectAttendedTransfer(attendedTransferSession: any) {
-    this.props.session.refer(attendedTransferSession)
-    phoneStore.dispatch({
-      type: SIPSESSION_ATTENDED_TRANSFER_SUCCESS
-    })
-    this.setState((attendedTransferSession = null))
-  }
-
-  cancelAttendedTransfer(attendedTransferSession: any) {
-    attendedTransferSession.cancel()
-    this.setState({ attendedTransferSessionPending: null })
-    this.setState({ attendedTransferSession: null })
-
-    //transfer cancel success action
-  }
-
-  checkTransferDialString() {
-    const number = this.state.transferDialString
-    if (parseInt(number, 10) && number.length === 10) {
-      return false
-    } else {
-      return true
-    }
-  }
-
   render() {
     const state = this.state
     return (
@@ -164,6 +57,7 @@ class Phone extends React.Component<Props> {
         <button disabled={state.ended} onClick={() => this.endCall()}>
           End Call
         </button>
+
         <Mute session={this.props.session} />
         <Hold session={this.props.session} />
         <input
@@ -171,36 +65,10 @@ class Phone extends React.Component<Props> {
             this.setState({ transferDialString: e.target.value })
           }
         />
-        <button
-          disabled={this.checkTransferDialString()}
-          onClick={() => this.attendedTransferCall()}
-        >
-          Attended Transfer Call
-        </button>
-        {this.state.attendedTransferSession ? (
-          <React.Fragment>
-            <button
-              onClick={() =>
-                this.connectAttendedTransfer(state.attendedTransferSession)
-              }
-            >
-              Connect Attended Transfer
-            </button>
-          </React.Fragment>
-        ) : null}
-        {state.attendedTransferSessionPending ? (
-          <React.Fragment>
-            <button
-              onClick={() =>
-                this.cancelAttendedTransfer(
-                  state.attendedTransferSessionPending
-                )
-              }
-            >
-              Cancel Attended Transfer
-            </button>
-          </React.Fragment>
-        ) : null}
+        <AttendedTransfer
+          destination={state.transferDialString}
+          session={this.props.session}
+        />
         <BlindTranfer
           destination={state.transferDialString}
           session={this.props.session}
