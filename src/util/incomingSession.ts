6c0b077706392f46e28e92e92a68e735918cab67
mxxnus <lxnkzr@gmail.com>
2020-06-16 16:36:05 -0400

    feat(code-quality): migrated audio functions to util

--- src/util/incomingSession.ts
+++ src/util/incomingSession.ts
@@ -6,106 +6,12 @@ import {
   SIPSESSION_HOLD_REQUEST,
   holdCallRequest
 } from '../actions/sipSessions'
-
-import {
-  REMOTE_AUDIO_CONNECTED,
-  LOCAL_AUDIO_CONNECTED
-} from '../actions/device'
+import { setLocalAudio, setRemoteAudio, cleanupMedia } from './audio'
 
 export class IncomingSessionStateHandler {
   private incomingSession: Session
   constructor(incomingSession: Session) {
     this.incomingSession = incomingSession
-  }
-
-  //creates new audio track then replaces audio track in getSender stream w/ new track
-  public setLocalAudioIncoming() {
-    const state = phoneStore.getState()
-
-    //@ts-ignore
-    const deviceId = state.device.primaryAudioInput
-
-    //@ts-ignore
-    this.incomingSession.sessionDescriptionHandler.peerConnection
-      .getSenders()
-      .forEach(function (sender: any) {
-        console.log(sender)
-        if (sender.track && sender.track.kind === 'audio') {
-          let audioDeviceId =
-            // audio input device_id
-            // 'default'
-            deviceId
-          navigator.mediaDevices
-            // @ts-ignore
-
-            //creates a stream w track
-            .getUserMedia({ audio: { deviceId: audioDeviceId } })
-            .then(function (stream) {
-              let audioTrack = stream.getAudioTracks()
-              console.log(audioTrack)
-              sender.replaceTrack(audioTrack[0])
-            })
-        }
-      })
-    phoneStore.dispatch({
-      type: LOCAL_AUDIO_CONNECTED
-    })
-  }
-
-  //adds track from getReceiver stream to <audio id={sessionId}> in Incoming.tsx
-  public setRemoteAudio() {
-    const state = phoneStore.getState()
-    //@ts-ignore
-    const deviceId = state.device.primaryAudioOutput
-
-    //@ts-ignore
-
-    const mediaElement = document.getElementById(this.incomingSession.id)
-
-    const remoteStream = new MediaStream()
-    //@ts-ignore
-    this.incomingSession.sessionDescriptionHandler.peerConnection
-      .getReceivers()
-      .forEach((receiver: any) => {
-        if (receiver.track) {
-          remoteStream.addTrack(receiver.track)
-        }
-      })
-    if (mediaElement) {
-      // @ts-ignore
-
-      console.log(mediaElement.sinkId)
-      // @ts-ignore
-
-      mediaElement.setSinkId(
-        // audio output device_id
-        // 'default'
-        deviceId
-      ).then(() => {
-        // @ts-ignore
-        mediaElement.srcObject = remoteStream
-        //@ts-ignore
-        mediaElement.play()
-      })
-
-
-    } else {
-      console.log('no media Element')
-    }
-    phoneStore.dispatch({
-      type: REMOTE_AUDIO_CONNECTED
-    })
-  }
-
-  public cleanupMedia() {
-    //@ts-ignore
-    const mediaElement: HTMLMediaElement = document.getElementById(this.incomingSession.id)
-    // @ts-ignore
-
-    mediaElement.srcObject = null
-    // @ts-ignore
-
-    mediaElement.pause()
   }
 
   public stateChange = (newState: SessionState) => {
@@ -120,19 +26,16 @@ export class IncomingSessionStateHandler {
           type: SIPSESSION_STATECHANGE,
           payload: { state: newState, id: this.incomingSession.id }
         })
-
-        //hold all sessions
         this.holdAll()
-        this.setLocalAudioIncoming()
-
-        this.setRemoteAudio()
+        setLocalAudio(this.incomingSession)
+        setRemoteAudio(this.incomingSession)
 
         break
       case SessionState.Terminating:
         phoneStore.dispatch({
           type: SIPSESSION_STATECHANGE
         })
-        this.cleanupMedia()
+        cleanupMedia(this.incomingSession.id)
         break
       case SessionState.Terminated:
         phoneStore.dispatch({
