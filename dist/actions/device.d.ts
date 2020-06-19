0e37de07ed40f3a6abeb1927e02af342c710f491
mxxnus <lxnkzr@gmail.com>
2020-06-19 16:03:25 -0400

    feature(device-manager): added set local and remote audio tracks to new deviceId action-types

--- dist/actions/device.d.ts
+++ dist/actions/device.d.ts
@@ -1,9 +1,16 @@
+import { Dispatch } from 'redux';
 export declare const AUDIO_INPUT_DEVICES_DETECTED = "AUDIO_INPUT_DEVICES_DETECTED";
 export declare const AUDIO_OUTPUT_DEVICES_DETECTED = "AUDIO_OUTPUT_DEVICES_DETECTED";
 export declare const REMOTE_AUDIO_CONNECTED = "REMOTE_AUDIO_CONNECTED";
 export declare const LOCAL_AUDIO_CONNECTED = "LOCAL_AUDIO_CONNECTED";
 export declare const SET_PRIMARY_OUTPUT = "SET_PRIMARY_OUTPUT";
 export declare const SET_PRIMARY_INPUT = "SET_PRIMARY_INPUT";
+export declare const SET_LOCAL_AUDIO_SESSIONS_PENDING = "LOCAL_AUDIO_SESSIONS_PENDING";
+export declare const SET_LOCAL_AUDIO_SESSIONS_SUCCESS = "LOCAL_AUDIO_SESSIONS_SUCCESS";
+export declare const SET_LOCAL_AUDIO_SESSIONS_FAIL = "LOCAL_AUDIO_SESSIONS_FAIL";
+export declare const SET_REMOTE_AUDIO_SESSIONS_PENDING = "SET_REMOTE_AUDIO_SESSIONS_PENDING";
+export declare const SET_REMOTE_AUDIO_SESSIONS_SUCCESS = "SET_REMOTE_AUDIO_SESSIONS_SUCCESS";
+export declare const SET_REMOTE_AUDIO_SESSIONS_FAIL = "SET_REMOTE_AUDIO_SESSIONS_FAIL";
 export declare const getInputAudioDevices: () => {
     type: string;
     payload: Object[];
@@ -20,3 +27,5 @@ export declare const setPrimaryInput: (id: string) => {
     type: string;
     payload: string;
 };
+export declare const setDeviceRemoteActiveSessions: () => (dispatch: Dispatch) => void;
+export declare const setDeviceLocalActiveSessions: () => (dispatch: Dispatch) => void;
--- src/actions/device.ts
+++ src/actions/device.ts
@@ -1,10 +1,21 @@
+import { Dispatch } from 'redux';
 
 export const AUDIO_INPUT_DEVICES_DETECTED = 'AUDIO_INPUT_DEVICES_DETECTED'
 export const AUDIO_OUTPUT_DEVICES_DETECTED = 'AUDIO_OUTPUT_DEVICES_DETECTED'
+
 export const REMOTE_AUDIO_CONNECTED = 'REMOTE_AUDIO_CONNECTED'
 export const LOCAL_AUDIO_CONNECTED = 'LOCAL_AUDIO_CONNECTED'
+
 export const SET_PRIMARY_OUTPUT = 'SET_PRIMARY_OUTPUT'
 export const SET_PRIMARY_INPUT = 'SET_PRIMARY_INPUT'
+
+export const SET_LOCAL_AUDIO_SESSIONS_PENDING = 'LOCAL_AUDIO_SESSIONS_PENDING'
+export const SET_LOCAL_AUDIO_SESSIONS_SUCCESS = 'LOCAL_AUDIO_SESSIONS_SUCCESS'
+export const SET_LOCAL_AUDIO_SESSIONS_FAIL = 'LOCAL_AUDIO_SESSIONS_FAIL'
+
+export const SET_REMOTE_AUDIO_SESSIONS_PENDING = 'SET_REMOTE_AUDIO_SESSIONS_PENDING'
+export const SET_REMOTE_AUDIO_SESSIONS_SUCCESS = 'SET_REMOTE_AUDIO_SESSIONS_SUCCESS'
+export const SET_REMOTE_AUDIO_SESSIONS_FAIL = 'SET_REMOTE_AUDIO_SESSIONS_FAIL'
 
 
 export const getInputAudioDevices = () => {
@@ -42,7 +53,8 @@ export const getOutputAudioDevices = () => {
 }
 
 export const setPrimaryOutput = (id: string) => {
-  // TODO other stuff for changing the device
+  //if amount of objects in sessions object is greater than 0 
+  //call setDeviceRemoteActiveSessions 
   return {
     type: SET_PRIMARY_OUTPUT,
     payload: id
@@ -50,13 +62,29 @@ export const setPrimaryOutput = (id: string) => {
 }
 
 export const setPrimaryInput = (id: string) => {
-  // TODO other stuff for changing the device
+  //if amount of objects in sessions object is greater than 0 
+  //call setDeviceLocalActiveSessions 
   return {
     type: SET_PRIMARY_INPUT,
     payload: id
   }
 }
 
+export const setDeviceRemoteActiveSessions = () => (dispatch: Dispatch) => {
+  //map thru session and get each stream.. 
+  //get audio html w/ sessionid 
+  //set new deviceId
+  dispatch({
+    type: SET_REMOTE_AUDIO_SESSIONS_PENDING
+  })
+}
 
-
-
+export const setDeviceLocalActiveSessions = () => (dispatch: Dispatch) => {
+  //map thru sessions
+  //create new stream with new track
+  //get senders stream and replace sender track with newly created track
+  //set new deviceId for track and play
+  dispatch({
+    type: SET_LOCAL_AUDIO_SESSIONS_PENDING
+  })
+}
