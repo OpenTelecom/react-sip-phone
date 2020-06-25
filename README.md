5df6cbd4c33ccbbbf70db23618c82facf8c48fe5
Jaonl <jason.lu@cloudpbx.ca>
2020-06-25 03:45:16 -0700

    feat(example): add example for triggering calls outside of component

--- README.md
+++ README.md
@@ -20,7 +20,37 @@ import 'react-sip-phone/dist/index.css'
 
 class Example extends Component {
   render() {
-    return <ReactSipPhone />
+    return <ReactSipPhone
+        name={name}
+        sipCredentials={{
+          sipuri: sipuri,
+          password: password
+        }}
+        sipConfig={{
+          websocket: websocket,
+          defaultCountryCode: '1'
+        }}
+        width={400}
+      />
+  }
+}
+```
+
+To make calls from outside of the component, import the phone's store and access the SIP Account once it is registered
+
+```tsx
+import { ReactSipPhone, phoneStore } from 'react-sip-phone'
+class MyApp extends Component {
+
+  makeCall(number) {
+    const sipAccount = phoneStore.getState().sipAccounts.sipAccount
+    if (sipAccount && number) {
+      sipAccount.makeCall(number)
+    }
+  }
+
+  render() {
+    return <ReactSipPhone ... />
   }
 }
 ```
--- example/src/App.js
+++ example/src/App.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { ReactSipPhone, reactSipPhoneReducers } from 'react-sip-phone'
+import { ReactSipPhone, phoneStore } from 'react-sip-phone'
 import 'react-sip-phone/dist/index.css'
 
 const urlParams = new URLSearchParams(window.location.search)
@@ -9,20 +9,34 @@ const password = urlParams.get('password')
 const websocket = urlParams.get('websocket')
 const name = urlParams.get('name')
 
+const outsideComponentDial = urlParams.get('dial')
+
 const App = () => {
+  const [dialstring, setDialstring] = useState('')
   return (
-    <ReactSipPhone
-      name={name || ''}
-      sipCredentials={{
-        sipuri: sipuri || '',
-        password: password || ''
-      }}
-      sipConfig={{
-        websocket: websocket || '',
-        defaultCountryCode: '1'
-      }}
-      width={0}
-    />
+    <React.Fragment>
+      {outsideComponentDial ? <div>
+        <input placeholder="Enter a number to dial"
+          onChange={(e) => setDialstring(e.target.value)} />
+        <button onClick={() => {
+          if (phoneStore.getState().sipAccounts.sipAccount && dialstring) {
+            phoneStore.getState().sipAccounts.sipAccount.makeCall(dialstring)
+          }
+        }}>Dial</button>
+      </div> : null}
+      <ReactSipPhone
+        name={name || ''}
+        sipCredentials={{
+          sipuri: sipuri || '',
+          password: password || ''
+        }}
+        sipConfig={{
+          websocket: websocket || '',
+          defaultCountryCode: '1'
+        }}
+        width={0}
+      />
+    </React.Fragment>
   )
 }
 
